d9853d5fb562979113964dbeed8cdf3ecd41ca84
Bondok6 <kirolousbondok6@gmail.com>
2022-02-14 05:39:13 +0200

    Responsive Filter || Add downArrow instead of default

--- src/components/products/ProductFilter.jsx
+++ src/components/products/ProductFilter.jsx
@@ -6,21 +6,21 @@ const ProductFilter = () => (
     <h3 className="filter-title">Filter</h3>
 
     <select name="Products" id="product">
-      <option value="product-value">product1</option>
+      <option value="product-value">product</option>
       <option value="product-value">product2</option>
       <option value="product-value">product3</option>
       <option value="product-value">product4</option>
     </select>
 
     <select name="State" id="state">
-      <option value="state-value">state1</option>
+      <option value="state-value">state</option>
       <option value="state-value">state2</option>
       <option value="state-value">state3</option>
       <option value="state-value">state4</option>
     </select>
 
     <select name="City" id="city">
-      <option value="city-value">city1</option>
+      <option value="city-value">city</option>
       <option value="city-value">city2</option>
       <option value="city-value">city3</option>
       <option value="city-value">city4</option>
